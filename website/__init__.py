1576869df0d7c0b5aad398a28ef4db3f610d1c84
Ray <rayxia2002@gmail.com>
2022-06-05 05:15:37 -0700

    database change

--- website/__init__.py
+++ website/__init__.py
@@ -6,16 +6,16 @@ import psycopg2
 import psycopg2.extras
 
 db = SQLAlchemy()
-DB_HOST = "final.c2hjl6vtbl0t.us-west-2.rds.amazonaws.com"
-DB_NAME = "dbfinal"
-DB_USER = "postgres"
-DB_PASS = "password"
+DB_HOST = "heffalump.db.elephantsql.com"
+DB_NAME = "psakykxm"
+DB_USER = "psakykxm"
+DB_PASS = "kPekxJYmEC08AUYrNkil_emLpVNx5bCz"
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 def create_app():
     app = Flask(__name__)
     app.secret_key = 'dsfdsf sdfsad fdsf'
     app.config['SECRET_KEY'] = 'sjdfiosfjrerge erg'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@final.c2hjl6vtbl0t.us-west-2.rds.amazonaws.com:5432/dbfinal'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://psakykxm:kPekxJYmEC08AUYrNkil_emLpVNx5bCz@heffalump.db.elephantsql.com/psakykxm'
     db.init_app(app)
 
     from .views import views
